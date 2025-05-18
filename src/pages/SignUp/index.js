014f508351d499683402f78176f001707b5f8710
leonardo <leonardo>
2025-05-18 17:02:14 -0300

    criado pagina login e cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,53 @@
 
+import { useState } from 'react'
+import './signup.css'
+
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
+
+
 export default function SignUp(){
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
   return(
-    <div>
-      <h1>Pagina Cadastro !</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistema de chamados" />
+        </div>
+
+        <form>
+          <h1>Fazer cadastro</h1>
+          <input 
+            type="text" 
+            placeholder="Digite seu nome..."
+            value={nome}
+            onChange={ (e) => setNome(e.target.value) }
+          />
+
+          <input 
+            type="text" 
+            placeholder="email@email.com"
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+
+          <input 
+            type="password" 
+            placeholder="********"
+            value={password}
+            onChange={ (e) => setPassword(e.target.value) }
+          />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        <Link to="/">Já possui uma conta? Faça login!</Link>
+
+      </div>
     </div>
   )
 }
